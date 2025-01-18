5fc737300acaeb0c0285cd2ed49ac01ac741f141
“Aamir <aamirhussainazad786@gmail.com>
2025-01-18 20:33:54 +0530

    commit add

--- L1Arrays/Array/Array/CreateArrays.java
+++ L1Arrays/Array/Array/CreateArrays.java
@@ -1,6 +1,6 @@
 package Array;
 
-public class Arrays {
+public class CreateArrays {
     public static void main(String[] args) {
 
         int[] arr = {10, 20, 30, 40, 50, 60, 70, 80, 90};
---
+++ L4Strings/StringRange.java
@@ -0,0 +1,7 @@
+public class StringRange {
+    public static void main(String[] args) {
+        char[] arr = {'j','a','v','a'};
+        String str = new String(arr, 0,3);
+        System.out.println(str);
+    }
+}
