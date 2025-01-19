f76803613ac56bab2d5eec0558966fa111d93590
“Aamir <aamirhussainazad786@gmail.com>
2025-01-19 22:11:47 +0530

    commit add

--- L4Strings/Buffers.java
+++ L4Strings/Buffers.java
@@ -4,6 +4,19 @@ public class Buffers {
     public static void main(String[] args) {
         StringBuffer sb = new StringBuffer("Hello");
 
+        System.out.println("Original String: " + sb);
+        System.out.println("Length: " + sb.length());
+        System.out.println("Capacity: " + sb.capacity());
+
+        System.out.println("Append: " + sb.append("  Google"));
+        System.out.println("Length: " + sb.length());
+        System.out.println("Capacity: " + sb.capacity());
+        
+        
+        sb.append(" Placement: ");
+        System.out.println("Length: " + sb.length());
+        System.out.println("Capacity: " + sb.capacity());
+
         // Thread 1
         Thread t1 = new Thread(() -> {
             sb.append(" World");
@@ -20,3 +33,6 @@ public class Buffers {
         t2.start();
     }
 }
+
+
+// BufferSize is the size of the internal buffer used by the StringBuffer.  16 is the default buffer size + 5 is the size of the hello size = 21
