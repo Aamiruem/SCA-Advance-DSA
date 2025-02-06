081b920ec2e8fdbb094d5ecd3bb9e6034464f81e
“Aamir <aamirhussainazad786@gmail.com>
2025-02-06 23:19:58 +0530

    commit add

---
+++ L6ArrayList/ArrayLists.java
@@ -0,0 +1,11 @@
+// package L6ArrayList;
+
+import java.util.ArrayList;
+public class ArrayLists {
+    public static void main(String[] args) {
+        // Create an ArrayList
+        ArrayList<Integer> list = new java.util.ArrayList<>();
+
+        // Add elements to the ArrayList
+    }
+}
